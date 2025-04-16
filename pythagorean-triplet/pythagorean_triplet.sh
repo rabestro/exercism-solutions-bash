e7ee9a8a7d4b6de07fc30affecee546b350ac5f9
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-16 09:12:48 +0300

    refactor algorithm

--- pythagorean-triplet/pythagorean_triplet.sh
+++ pythagorean-triplet/pythagorean_triplet.sh
@@ -6,16 +6,14 @@ main () {
   ((sum % 2 == 0)) || return 0
 
   local -ri limit=$(( sum / 3 ))
-  local -i a b c numerator denominator remainder
+  local -i a b c numerator denominator
 
   for (( a=SMALLEST_POSSIBLE_A; a <= limit; a++ ))
   do
       (( numerator = sum * sum - 2 * sum * a ))
       (( denominator = 2 * (sum - a) ))
-      (( remainder = numerator % denominator ))
-      (( remainder == 0 )) || continue
       (( b = numerator / denominator ))
-      (( a < b )) || continue
+      (( b * denominator == numerator && a < b )) || continue
       (( c = sum - a - b ))
       (( c > b )) || return 0
       printf "%d,%d,%d\n" $a $b $c
